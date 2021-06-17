a018d87c5cfc72bc147b0ec1774e70abe07485e6
seodea <46041493+seodea@users.noreply.github.com>
2021-06-17 10:11:52 +0900

    Add SG instance info

--- dev/output.tf
+++ dev/output.tf
@@ -1,27 +1,36 @@
-
 output "vpc_info" {
 
-	value = module.dev_vpc
+        value = module.dev_vpc
 }
 
 output "public_sg_info" {
 
-	value = module.public_sg
+        value = module.public_sg
 }
 
 output "was_sg_info" {
 
-	value = module.was_sg
+        value = module.was_sg
 }
 
 output "web_instances" {
-	value = module.web_instances
+        value = module.web_instances
 
 }
 
 
 output "was_instances" {
-	value = module.was_instances
+        value = module.was_instances
+
+}
+
+output "was_slb_instance" {
+        value = module.dev_internal_slb
+
+}
+
+output "web_slb_instance" {
+        value = module.dev_public_slb
 
 }
 
@@ -30,4 +39,3 @@ output "db_info" {
   value = module.mysql
 
 }
-
